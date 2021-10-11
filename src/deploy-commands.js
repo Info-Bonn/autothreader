40ab9bdedee23bb5691bbbd896892541aa47d449
Finn Kalvelage <f.kalvelage@posteo.de>
2021-10-11 18:23:36 +0200

    add check for guildid

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -8,11 +8,23 @@ commands.push(require("./register_commands/disable"));
 commands.push(require("./register_commands/list"));
 
 const rest = new REST({ version: "9" }).setToken(process.env.TOKEN);
-// register them with discord as guild commands
-rest
-  .put(
-    Routes.applicationGuildCommands(process.env.CLIENTID, process.env.GUILDID),
-    { body: commands }
-  )
-  .then(() => console.log("Successfully registered application commands."))
-  .catch(console.error);
+
+if (process.env.GUILDID) {
+  // register them with discord as global commands
+  rest
+    .put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENTID,
+        process.env.GUILDID
+      ),
+      { body: commands }
+    )
+    .then(() => console.log("Successfully registered application commands."))
+    .catch(console.error);
+} else {
+  // register them with discord as guild commands
+  rest
+    .put(Routes.applicationCommands(process.env.CLIENTID), { body: commands })
+    .then(() => console.log("Successfully registered application commands."))
+    .catch(console.error);
+}
